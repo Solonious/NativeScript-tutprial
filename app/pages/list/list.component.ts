f0841fff9848bbc58b3a369b758c56adf8ab4bef
Sergii Solonar <sergeysolonar@gmail.com>
2017-07-28 08:34:52 +0300

    added loader indicator

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -11,17 +11,22 @@ import { TextField } from "ui/text-field";
 })
 export class ListComponent implements OnInit {
   groceryList: Array<Grocery> = [];
-    grocery = "";
-    @ViewChild("groceryTextField") groceryTextField: ElementRef;
+  grocery = "";
+  isLoading = false;
+  listLoaded = false;
+  @ViewChild("groceryTextField") groceryTextField: ElementRef;
 
   constructor(private groceryListService: GroceryListService) {}
 
   ngOnInit() {
+      this.isLoading = true;
     this.groceryListService.load()
       .subscribe(loadedGroceries => {
         loadedGroceries.forEach((groceryObject) => {
           this.groceryList.unshift(groceryObject);
         });
+        this.isLoading = false;
+        this.listLoaded = true;
       });
   }
     add() {
